c1b41caf0b9b153bc14942d4198b1c8e9d80b467
Aashutosh <aryalaashutosh25@gmail.com>
2019-12-07 12:20:12 +0545

    Add stack implementation
    Implementation of Stack using a singly Linked List.

---
+++ Linked List/stack.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include "stack.h"
+#define MAX 5
+
+using std :: cout;
+using std :: endl;
+
+int count {0};
+
+Stack :: Stack(){
+    HEAD = NULL;
+}
+
+Stack :: ~Stack(){};
+
+bool Stack :: isEmpty(){
+    return (HEAD == NULL);
+}
+
+void Stack :: push(int data){
+    if (count == MAX){
+        cout << "Stack overflow!" << endl;
+    }
+    else{
+        Node *newNode = new Node();
+        newNode -> info = data;
+        newNode -> next = HEAD;
+        HEAD = newNode;
+    }
+    count++;
+}
+
+int Stack :: pop(){
+    if (isEmpty()){
+        cout << "Stack Underflow" << endl;
+        return -1;
+    }
+    else{
+        Node *toDelete;
+        int value;
+        toDelete = HEAD;
+        value = toDelete -> info;
+        HEAD = toDelete -> next;
+        delete toDelete;
+        return value;
+    }
+    count--;
+}
+
+int Stack :: top(){
+    if(isEmpty()){
+        cout << "Stack Underflow" << endl;
+        return -1;
+    }
+    else{
+        return (HEAD -> info);
+    }
+}
+
+void Stack :: traverse(){
+    Node *temp = HEAD;
+    while (temp != NULL)
+    {
+        cout << temp -> info << endl;
+        temp = temp -> next;
+    }
+}
+
+int main(){
+    Stack S;
+    cout << S.isEmpty() <<endl;
+    cout << "*****************************"  << endl;
+    S.push(2);
+    S.push(4);
+    S.push(6);
+    S.push(8);
+    S.push(10);
+    S.traverse();
+    S.push(69);
+    cout << "*****************************"  << endl;
+    cout << S.top() << endl;
+    cout << S.pop() <<endl;
+    cout << S.top() << endl;
+    cout << S.pop() <<endl;
+    cout << "*****************************"  << endl;
+    S.traverse();
+    return 0;
+}
